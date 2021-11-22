f5411fc8f92f2ce1c5ea10c1ae4224a896f61c6f
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-11-22 15:34:44 -0700

    test

---
+++ javaCourseExamples/src/javaCourseExamples/javaArrays.java
@@ -0,0 +1,36 @@
+package javaCourseExamples;
+
+public class javaArrays {
+	
+	int[] intArrayOne;
+	int intArrayTwo[];
+	//both are okay, but first one is conventional ^
+	
+	char[] charArrayOne, charArrayTwo;
+	//both are arrays
+	
+	int[] intArray1 = new int[10];
+	
+	int[] intArray2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	int[] intArray; 
+	
+	//length of the array is a read only value, cannot say "array.length = 10" 
+	
+	int[] tempaArray = {1,2,3};
+	
+	
+	//you can use polymorphism to create an array of multiples types
+
+	
+	String[][] multiArray = new String[5][2];
+//	multiArray[1][0] = new String["Ben"];
+//	multiArray[0][0] = "Paul";
+//	multiArray[0][1] = "Pauls phone #";
+//	multiArray[1][0] = "Ben";
+//	multiArray[0][1] = "Ben's phone #";
+
+//	//multi-dimensional array &^ 
+//	
+
+}
